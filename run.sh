5768c5d68579193fe571253695480782ab34c921
JadsonSS12 <jadson.w123@gmail.com>
2026-04-18 16:12:03 -0300

    java imports added

--- run.sh
+++ run.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 
-# Caminhos onde estao os workloads
-PYTHON_JMX="/home/jadson/Downloads/TestsAging/python/http_test.jmx"
-JS_JMX="/home/jadson/Downloads/TestsAging/js/http_test.jmx"
 
-# Caminho para o executável do JMeter
+export SDKMAN_DIR="$HOME/.sdkman"
+[[ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]] && source "$SDKMAN_DIR/bin/sdkman-init.sh"
+
+export JAVA_HOME="/root/.sdkman/candidates/java/17.0.10-tem"
+export JRE_HOME="$JAVA_HOME"
+export PATH=$JAVA_HOME/bin:$PATH
+
+# arquivos workload
+PYTHON_JMX="/home/jadson/Downloads/TestsAging/py/test_js.jmx"
+
+JS_JMX="/home/jadson/Downloads/TestsAging/js/test_python.jmx"
+
+
+# Caminhodo Jemter
 JMETER_BIN="/home/jadson/Downloads/TestsAging/js/apache-jmeter-5.6.3/bin/jmeter.sh"
 
 
 stop_workload() {
     echo -e "\n[!] Interrupção detectada. Encerrando workloads..."
-    # Mata os processos
-    kill $PYTHON_PID $JS_PID 2>/dev/null
+   
+    kill $PYTHON_PID 2>/dev/null
+    
+    kill $JS_PID 2>/dev/null
+
+    
     echo "[+] Testes parados com sucesso."
     exit 0
 }
@@ -19,13 +33,15 @@ stop_workload() {
 
 trap stop_workload SIGINT
 
-#echo "Iniciando Python "
+echo "Iniciando Python "
 $JMETER_BIN -n -t "$PYTHON_JMX" -l /dev/null > /dev/null 2>&1 &
 PYTHON_PID=$!
 
-echo "Iniciando carga para Aplicação JS (Porta 8081)..."
+
+echo "Iniciando js"
 $JMETER_BIN -n -t "$JS_JMX" -l /dev/null > /dev/null 2>&1 &
 JS_PID=$!
+
 
 echo "----------------------------------------------------"
 echo "PID Python: $PYTHON_PID | PID JS: $JS_PID"
